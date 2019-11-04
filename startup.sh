ec2406be6af132058f214976cc01d5c19acaee7d
Sean Capaloff-Jones <seancjones@google.com>
2019-11-04 16:02:04 -0500

    store

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 pip3 install pipenv --user
+gcloud config set project $1
 gcloud services enable doubleclicksearch.googleapis.com
 gcloud services enable storage-component.googleapis.com
 gcloud services enable bigquery-json.googleapis.com
 gcloud services enable bigquerystorage.googleapis.com
-if [[ $(grep -c "$HOME/.local/bin" <(echo $PATH)) == 0 ]]
+if [[ -z $(which pipenv) ]]
 then
     echo "PATH=\$PATH:$HOME/.local/bin" >> $HOME/.bashrc
     source ~/.bashrc
-    gcloud config set project $1
 fi
