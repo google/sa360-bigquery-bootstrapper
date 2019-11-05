74578143dcf14ce07ee61a14d7f3844b1d19845b
Sean Capaloff-Jones <seancjones@google.com>
2019-11-04 20:19:11 -0500

    store

--- bootstrapper.py
+++ bootstrapper.py
@@ -106,6 +106,7 @@ class Bootstrap:
         s_cli = self.settings.custom['storage_client']  # type: storage.Client
         bucket = s_cli.get_bucket(self.settings['storage_bucket'].value)
         blob = bucket.blob(blob_name=file)
+        blob.name = file
         result = blob.download_as_string(s_cli, 0, 1000)
         rows = result.decode().split('\n')[0:2]
         schema = []
