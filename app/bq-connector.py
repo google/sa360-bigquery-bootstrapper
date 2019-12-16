d7e348081a81f10c4bc9dbb1f8f516922fd1feae
Sean Capaloff-Jones <seancjones@google.com>
2019-12-16 12:19:35 -0500

    bootstrap for web

---
+++ app/bq-connector.py
@@ -0,0 +1,13 @@
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
