4637b45a3b0ea3eb0aab7b77ae9e5a269ec2434e
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 04:30:31 +0000

    Adding tarfile member sanitization to extractall()

--- csv_decoder.py
+++ csv_decoder.py
@@ -300,7 +300,26 @@ class Decoder(object):
         def run(self):
             with tarfile.open(self.path) as th:
                 extraction_directory = '/tmp/tar-output-' + self.parent.time
-                th.extractall(extraction_directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(th, extraction_directory)
                 Decoder.DirectoryDecoder(self.parent, extraction_directory).run()
                 shutil.rmtree(extraction_directory)
 
